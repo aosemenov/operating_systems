d9af62853be8697aa12e941b1c2341a5b5e05ab5
aosemenov_1 <aosemenov_1@edu.hse.ru>
2024-12-10 15:20:54 +0300

    bash-3

--- bash/test.sh
+++ bash/test.sh
@@ -5,19 +5,13 @@ CGROUP_NAME="test2"
 MEMORY_LIMIT="60M"
 CPU_MAX="50000" 
 
-# Проверка, что cgroup смонтирован
-if ! grep -q cgroup /proc/mounts; then
-    echo "Cgroup не смонтирован. Убедитесь, что cgroup включен."
-    exit 1
-fi
-
 # Создание группы cgroup
 sudo cgcreate -a $USER -t $USER -g memory,cpu:$CGROUP_NAME
 
 # Установка ограничений на память и CPU
 sudo cgset -r memory.max=$MEMORY_LIMIT $CGROUP_NAME
 sudo cgset -r memory.hight=$MEMORY_LIMIT $CGROUP_NAME
-sudo cgset -r cpu.max=$CPU_SHARES $CGROUP_NAME
+sudo cgset -r cpu.max=$CPU_MAX $CGROUP_NAME
 
 # Проверка значений
 echo "Ограничения для группы $CGROUP_NAME:"
@@ -26,10 +20,10 @@ cgget -r cpu.max $CGROUP_NAME
 
 # Запуск ресурсоемкого процесса в группе
 echo "Запуск процесса stress в группе cgroup..."
-cgexec -g memory,cpu:$CGROUP_NAME stress --vm 1 --vm-bytes 128M --vm-keep --timeout 20s &
+sudo cgexec -g memory,cpu:$CGROUP_NAME stress --cpu 2 --vm 1 --vm-bytes 128M --timeout 20s &
 
 # Ожидание завершения процесса
-wait
+sleep 30
 
 # Очистка: удаление группы cgroup
 sudo cgdelete -r memory,cpu:$CGROUP_NAME
