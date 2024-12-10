0a07fed784e1200120adce58d7aa7b5343b96622
aosemenov_1 <aosemenov_1@edu.hse.ru>
2024-12-10 15:28:06 +0300

    bash-4

--- bash/test.sh
+++ bash/test.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-# Имя группы cgroup и ограничение на память
 CGROUP_NAME="test2"
 MEMORY_LIMIT="60M"
 CPU_MAX="50000" 
 
-# Создание группы cgroup
+
 sudo cgcreate -a $USER -t $USER -g memory,cpu:$CGROUP_NAME
 
-# Установка ограничений на память и CPU
 sudo cgset -r memory.max=$MEMORY_LIMIT $CGROUP_NAME
-sudo cgset -r memory.hight=$MEMORY_LIMIT $CGROUP_NAME
 sudo cgset -r cpu.max=$CPU_MAX $CGROUP_NAME
 
-# Проверка значений
 echo "Ограничения для группы $CGROUP_NAME:"
 cgget -r memory.max $CGROUP_NAME
 cgget -r cpu.max $CGROUP_NAME
 
-# Запуск ресурсоемкого процесса в группе
 echo "Запуск процесса stress в группе cgroup..."
-sudo cgexec -g memory,cpu:$CGROUP_NAME stress --cpu 2 --vm 1 --vm-bytes 128M --timeout 20s &
+sudo cgexec -g memory,cpu:$CGROUP_NAME stress --cpu 2 --vm 1 --vm-bytes 3000M --timeout 20s &
 
-# Ожидание завершения процесса
 sleep 30
 
-# Очистка: удаление группы cgroup
 sudo cgdelete -r memory,cpu:$CGROUP_NAME
-
-# Сообщение об успешном завершении
 echo "Скрипт завершен. Cgroup удален."
